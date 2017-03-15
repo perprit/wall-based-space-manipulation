0dcec0511096986d89f08a1574c3c21c6a86da42
jihwang <jihwang@hcil.snu.ac.kr>
2017-03-15 19:50:48 +0900

    Debug text

---
+++ Assets/Scripts/DebugTextController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ManipulateWalls
+{
+    public class DebugTextController : MonoBehaviour
+    {
+
+        Text text;
+
+        // Use this for initialization
+        void Start()
+        {
+            text = gameObject.GetComponent<Text>();
+        }
+
+        void Update()
+        {
+        }
+
+        public void SetMessage(string message, LogType type = LogType.Log)
+        {
+            text.text = message;
+        }
+
+        public void AddMessage(string message, LogType type = LogType.Log)
+        {
+            text.text += "\n" + message;
+        }
+
+        public void ClearMessage()
+        {
+            text.text = "";
+        }
+    }
+}
